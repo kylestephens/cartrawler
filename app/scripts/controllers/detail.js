e03631571369d6558563866a62aefce132abc325
Kyle Stephens <Kyle_Stephens@mastercard.com>
2016-02-27 13:16:39 +0000

    Further updates to detail view

--- app/scripts/controllers/detail.js
+++ app/scripts/controllers/detail.js
@@ -9,10 +9,18 @@
  */
 angular.module('cartrawlerApp')
 .controller('DetailCtrl', function($scope, $rootScope, $location, BookingService) {
+
+	/**
+	 * Referenced from Booking Service
+	 * 
+	 * If this was a real world scenario, there would ideally be a separate endpoint
+	 * to retrieve more specific Vehicle information
+	 * 
+	 * The vehicle detail view would then be able to call this endpoint, via the Service,
+	 * and reference this vehicle by id, e.g.:
+	 * 		http://<baseurl>/#/detail/<vehicle_id>
+	 */
     $scope.vehicle = BookingService.data.selectedVehicle;
 
-    $scope.onGoBack = function() {
-    	$location.path( "/" );
-    }
 });
 
---
+++ test/README.md
@@ -0,0 +1,3 @@
+Jasmine unit tests would go in here.
+
+These would be executed via Grunt task.
